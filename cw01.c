86743d28fd4ad78121bba7703669e1c057681ed7
JanPud <80970205+JanPud@users.noreply.github.com>
2021-04-21 11:10:00 +0200

    Update cw01.c

--- cw01.c
+++ cw01.c
@@ -12,9 +12,5 @@ printf("\n wynik z %.2f - %.2f = %.2f \n", a, b, a-b );
 printf("\n wynik z %.2f * %.2f = %.2f \n", a, b, a*b );
 printf("\n wynik z %.2f / %.2f = %.2f \n", a, b, a/b );
 
-a=a*b;
-
-printf("nowe a = %.2f", a);
-
 return 0;
 }
